4f6ed89ffb77a635a9f6353e66ab5514f5a3f735
poorna-theekshana <it21218958@my.sliit.lk>
2025-04-18 00:06:26 +0530

    Update server port to 8000

--- auth-service/server.js
+++ auth-service/server.js
@@ -22,7 +22,7 @@ const userRoutes = require("./routes/userRoutes");
 
 // Initialize Express App
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
