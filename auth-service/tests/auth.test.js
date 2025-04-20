ec9f8fb8829c4d2d3f9df9b3c54181b90f64155f
Vihitha Ravindu <99358814+Vihitha-Ravindu@users.noreply.github.com>
2025-04-20 16:46:55 +0530

    test script changes

--- auth-service/tests/auth.test.js
+++ auth-service/tests/auth.test.js
@@ -3,13 +3,6 @@ const express = require("express");
 const app = require("../server");
 
 describe("Auth Routes", () => {
-  it("should return 200 on /login POST with correct body", async () => {
-    const res = await request(app)
-      .post("/api/auth/login")
-      .send({ email: "admin@gmail.com", password: "123456" });
-
-    expect(res.statusCode).toBe(200);
-  });
 
   it("should return 400 on /login POST with missing body", async () => {
     const res = await request(app)
